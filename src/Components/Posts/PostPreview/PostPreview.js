2f711a2e037fcc55af43da8c37ada59240d12540
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-01-01 21:25:14 +0000

    PostDate refactoring

--- src/Components/Posts/PostPreview/PostPreview.js
+++ src/Components/Posts/PostPreview/PostPreview.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
 import './PostPreview.css'
+import PostDate from '../PostDate'
 
 export default class PostPreview extends Component {
   render() {
     return (
       <div className="PostPreview">
-        <h3 className="mb-0" dangerouslySetInnerHTML={{ __html: this.props.title }}>
-        </h3>
+        <Link to={this.props.link} className="title">
+            <h3 className="mb-0" dangerouslySetInnerHTML={{ __html: this.props.title }}>
+            </h3>
+        </Link>
         <p className="mt-0">
           <small>
-            {
-              new Date(this.props.datePublished).toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
-            }
+              <PostDate date={this.props.datePublished} />
           </small>
         </p>
         <span>
