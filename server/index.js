52bd31aae69d9d9681b050a4ca133fa9d21058e5
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-05-10 12:57:58 +0000

    fix port issue for heroku deploy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = 3000
+const PORT =  process.env.PORT ? process.env.PORT : 3000
 
-const reactRenderer = require('./react-renderer');
-const routes = ['/', '/posts', '/posts/:slug'];
+const reactRenderer = require('./react-renderer')
+const routes = ['/', '/posts', '/posts/:slug']
 
 const app = express()
 
