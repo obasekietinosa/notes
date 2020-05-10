ba0fc5dd7cdc1f7a3c526af8ee80818d1da67556
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-05-10 10:39:57 +0000

    Heroku Serve SSR
    Remove Firebase Functions. Commit Procfile

--- functions/src/Components/Contexts/BlogContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-const BlogContext = createContext()
-
-export const BlogProvider = BlogContext.Provider
-export const BlogConsumer = BlogContext.Consumer
-
-export default BlogContext
