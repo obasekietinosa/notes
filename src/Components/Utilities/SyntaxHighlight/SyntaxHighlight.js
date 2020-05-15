20509a8b37a902809cff192896325f53cd9b691f
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-05-15 07:22:48 +0000

    Update Syntax Highlighting to use Prism

--- src/Components/Utilities/SyntaxHighlight/SyntaxHighlight.js
+++ src/Components/Utilities/SyntaxHighlight/SyntaxHighlight.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
-import hljs from 'highlight.js/lib/highlight'
-import javascript from 'highlight.js/lib/languages/javascript'
-
-import 'highlight.js/styles/atelier-estuary-dark.css'
-
-hljs.registerLanguage('javascript', javascript);
+import Prism from "prismjs"
+import './prism.css'
 
 export default class SyntaxHighlight extends Component {
     constructor(props) {
@@ -13,20 +9,11 @@ export default class SyntaxHighlight extends Component {
     }
 
     componentDidMount() {
-        this.highlight()
+      Prism.highlightAll()
     }
 
     componentDidUpdate() {
-        this.highlight()
-    }
-
-    highlight = () => {
-        if (this.nodeRef) {
-            const nodes = this.nodeRef.current.querySelectorAll('pre')
-            nodes.forEach((node) => {
-                hljs.highlightBlock(node)
-            })
-        }
+      Prism.highlightAll()
     }
 
     render() {
